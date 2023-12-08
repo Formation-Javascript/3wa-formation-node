495a1e516a4c4e6eef6642a51252fea1c7703761
MentorMindOtaku <contact@seckmamadou.com>
2023-12-08 17:03:36 +0100

    version 1

--- exercises/lottery/index.js
+++
@@ -1,9 +0,0 @@
-const chalk = require('chalk');
-
-const { loto, gagnant } = require('./modules/lottery');
-
-console.log(loto({ min: 20, max: 50, count: 5 }));
-
-const players = ['Kali', 'Kim', 'Jules', 'Timo'];
-
-console.log(chalk.green(`Le gagnant(e) est : ${gagnant(players)}`));
